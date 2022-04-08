341e3a18f3fecea8f1bbd39b01bf9c68a18c0bcc
Moopz-Dev <pongzthor@gmail.com>
2022-04-08 09:21:14 +0700

    feat: adjust size and position of program window

---
+++ program.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+# initialize an instance of TKINTER , and naming the title
+root = Tk()
+root.title("My GUI")
+
+
+# program window size and position ("{height}x{weight}+{x-offset}+{y-offset}")
+root.geometry("640x480+50+50")
+
+
+root.mainloop()
